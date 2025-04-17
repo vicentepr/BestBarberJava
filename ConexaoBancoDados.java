53429837853bc7a6b87105f780e6edcd8716d5b4
vicentepr <140712692+vicentepr@users.noreply.github.com>
2025-04-17 14:57:49 -0300

    Update ConexaoBancoDados.java

--- ConexaoBancoDados.java
+++ ConexaoBancoDados.java
@@ -1,8 +1,10 @@
+package Repository;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexaoBancoDados {
+public class ConexaoBancoDados1 {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASSWORD = "12345";
